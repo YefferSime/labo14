f0217731029c0340028ab1c20bac8df5413c439d
YefferSime <138342642+YefferSime@users.noreply.github.com>
2024-11-26 21:34:14 -0500

    Update StudentInsertRequestV6.cs
    cambios

--- Requests/StudentInsertRequestV6.cs
+++ Requests/StudentInsertRequestV6.cs
@@ -3,6 +3,6 @@
     public class StudentInsertRequestV6
     {
         public int IdGrade { get; set; }
-        public List<StudentRequestV1> Students { get; set; }  // Usamos el modelo StudentRequestV1 para representar a cada estudiante
+        public List<StudentRequestV1> Students { get; set; }
     }
 }
